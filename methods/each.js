70b825fccb56c3e0525417c796faf899e46ad90e
Nik Ahmad Z <7868782+nikahmadz@users.noreply.github.com>
2019-10-20 20:16:58 +0800

    Update each.js

--- methods/each.js
+++ methods/each.js
@@ -1,4 +1,6 @@
-const each=function(list,index,step,max,call){if(index>max||call.call(this,index,list[index])===false){return}each.call(this,list,index+step,step,max,call)};
+function each(list,index,step,max,call){if(index>max||call.call(this,index,list[index])===false){return}each.call(this,list,index+step,step,max,call)};
+function each(o,x,c){if(c){each.call(this,o,x++,x<xx)}}
+
 each(TQS,0,1,TQS.length-1,function(index,item){
   console.log(item,index);
 })
